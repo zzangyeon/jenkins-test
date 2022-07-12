8df5be8ae17f10647cef4c537b63f85aa0507fd0
zzangyeon <79262461+zzangyeon@users.noreply.github.com>
2022-07-12 18:23:01 +0900

    Add files via upload

---
+++ src/main/java/com/jenkins/jen/JenApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/jen/JenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
